fcd1c56686f3e1eb37baad6ba06690331c1841ab
Sidney Kanyi <sidney.kanyi@strathmore.edu>
2021-08-13 21:59:32 +0300

    initialized and successfully created db model

--- app.py
+++ app.py
@@ -15,3 +15,29 @@ class User(db.Model):
     id = db.Column(db.Integer,primary_key=True)
     email = db.Column(db.String(200), nullable=False)
     password = db.Column(db.String(200), nullable=False)
+   
+    def __repr__(self):
+        return '<User %r>'% self.id
+
+@app.route('/')
+def index():
+    return render_template("index.html")
+
+@app.route('/login', methods=['POST', 'GET'])
+def login():
+
+    if request.method == "POST":
+
+        login = User(email= request.form['email'], password=request.form['password'])
+
+        try:
+            db.session.add(login)
+            db.session.commit()
+            return redirect('/')
+        except:
+            return "Invalid Credentials"
+    else:
+        return render_template("login.html")
+
+if __name__ == '__main__':
+    app.run(debug = True)
