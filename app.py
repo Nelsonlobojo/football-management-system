c2b8f97c469208d93c343a974dc98d264cdcfeae
Nelson Lobor <nelson.lobojo@strathmore.edu>
2021-08-18 00:06:23 +0300

    login works sucessfully!!!

--- app.py
+++ app.py
@@ -3,8 +3,10 @@ from flask_mysqldb import MySQL
 import MySQLdb.cursors
 import re
 
-
 app = Flask(__name__)
+
+# Change this to your secret key (can be anything, it's for extra protection)
+app.secret_key = 'bacon'
 
 
 # Enter your database connection details below
@@ -15,12 +17,6 @@ app.config['MYSQL_DB'] = 'pythonlogin'
 
 # Intialize MySQL
 mysql = MySQL(app)
-
-
-
-
-
-
 
 @app.route('/')
 def landing():
@@ -34,11 +30,11 @@ def login():
         # Check if "username" and "password" POST requests exist (user submitted form)
     if request.method == 'POST' and 'email' in request.form and 'password' in request.form:
         # Create variables for easy access
-        username = request.form['email']
+        email = request.form['email']
         password = request.form['password']
                 # Check if account exists using MySQL
         cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
-        cursor.execute('SELECT * FROM accounts WHERE email = %s AND password = %s', (username, password,))
+        cursor.execute('SELECT * FROM accounts WHERE email = %s AND password = %s', (email, password,))
         # Fetch one record and return result
         account = cursor.fetchone()
             # If account exists in accounts table in out database
@@ -46,48 +42,40 @@ def login():
             # Create session data, we can access this data in other routes
             session['loggedin'] = True
             session['id'] = account['id']
-            session['username'] = account['username']
+            session['email'] = account['email']
             # Redirect to home page
-            return 'Logged in successfully!'
+            return render_template('layout.html')
         else:
             # Account doesnt exist or username/password incorrect
             msg = 'Incorrect username/password!'
+    return render_template('login.html', msg=msg)
 
         
 # logout page
-@app.route('/login/logout')
-def logout():
+#@app.route('/login/logout')
+#def logout():
     # Remove session data, this will log the user out
-   session.pop('loggedin', None)
-   session.pop('id', None)
-   session.pop('username', None)
+  # session.pop('loggedin', None)
+   #session.pop('id', None)
+   #session.pop('username', None)
    
    # Redirect to login page
-   return redirect(url_for('login'))
+   #return redirect(url_for('login'))
 #this will be the home page, only accessible for loggedin users
-@app.route('/login/home')
-def home():
-    # Check if user is loggedin
-    if 'loggedin' in session:
-        # User is loggedin show them the home page
-        return render_template('home.html', username=session['username'])
-    # User is not loggedin redirect to login page
-    return redirect(url_for('login'))
 
-    #this will be the profile page, only accessible for loggedin users
 @app.route('/login/profile')
 def profile():
     # Check if user is loggedin
     if 'loggedin' in session:
         # We need all the account info for the user so we can display it on the profile page
 
-        cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
-        cursor.execute('SELECT * FROM accounts WHERE id = %s', (session['id'],))
-        account = cursor.fetchone()
+       cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+       cursor.execute('SELECT * FROM accounts WHERE id = %s', (session['id'],))
+       account = cursor.fetchone()
 
         # Show the profile page with account info
 
-        return render_template('profile.html', account=account)
+       return render_template('layout.html', account=account)
 # User is not loggedin redirect to login page
 
     return redirect(url_for('login'))
