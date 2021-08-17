08112e1e6c9ae600d91c024e745ba55b2060f789
Sidney Kanyi <sidney.kanyi@strathmore.edu>
2021-08-17 14:11:02 +0300

    complete login html code

--- app.py
+++ app.py
@@ -1,23 +1,26 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for, session
+from flask_mysqldb import MySQL
+import MySQLdb.cursors
+import re
 
-#object relational mapper for python
-from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///login.db'
 
-#initializing the db with app settings
-db = SQLAlchemy(app)
+# Enter your database connection details below
+app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = ''
+app.config['MYSQL_DB'] = 'pythonlogin'
 
-#Create the db model
-class User(db.Model):
-    id = db.Column(db.Integer,primary_key=True)
-    email = db.Column(db.String(200), nullable=False)
-    password = db.Column(db.String(200), nullable=False)
-   
-    def __repr__(self):
-        return '<User %r>'% self.id
+# Intialize MySQL
+mysql = MySQL(app)
+
+
+
+
+
+
 
 @app.route('/')
 def index():
@@ -25,19 +28,69 @@ def index():
 
 @app.route('/login', methods=['POST', 'GET'])
 def login():
+    #Output message if something goes wrong...
+    msg= 'Cannot find page'
 
-    if request.method == "POST":
+        # Check if "username" and "password" POST requests exist (user submitted form)
+    if request.method == 'POST' and 'email' in request.form and 'password' in request.form:
+        # Create variables for easy access
+        username = request.form['email']
+        password = request.form['password']
+                # Check if account exists using MySQL
+        cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+        cursor.execute('SELECT * FROM accounts WHERE email = %s AND password = %s', (username, password,))
+        # Fetch one record and return result
+        account = cursor.fetchone()
+            # If account exists in accounts table in out database
+        if account:
+            # Create session data, we can access this data in other routes
+            session['loggedin'] = True
+            session['id'] = account['id']
+            session['username'] = account['username']
+            # Redirect to home page
+            return 'Logged in successfully!'
+        else:
+            # Account doesnt exist or username/password incorrect
+            msg = 'Incorrect username/password!'
 
-        login = User(email= request.form['email'], password=request.form['password'])
+        
+# logout page
+@app.route('/login/logout')
+def logout():
+    # Remove session data, this will log the user out
+   session.pop('loggedin', None)
+   session.pop('id', None)
+   session.pop('username', None)
+   
+   # Redirect to login page
+   return redirect(url_for('login'))
+#this will be the home page, only accessible for loggedin users
+@app.route('/login/home')
+def home():
+    # Check if user is loggedin
+    if 'loggedin' in session:
+        # User is loggedin show them the home page
+        return render_template('home.html', username=session['username'])
+    # User is not loggedin redirect to login page
+    return redirect(url_for('login'))
 
-        try:
-            db.session.add(login)
-            db.session.commit()
-            return redirect('/')
-        except:
-            return "Invalid Credentials"
-    else:
-        return render_template("login.html")
+    #this will be the profile page, only accessible for loggedin users
+@app.route('/login/profile')
+def profile():
+    # Check if user is loggedin
+    if 'loggedin' in session:
+        # We need all the account info for the user so we can display it on the profile page
+
+        cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+        cursor.execute('SELECT * FROM accounts WHERE id = %s', (session['id'],))
+        account = cursor.fetchone()
+
+        # Show the profile page with account info
+
+        return render_template('profile.html', account=account)
+# User is not loggedin redirect to login page
+
+    return redirect(url_for('login'))
 
 if __name__ == '__main__':
     app.run(debug = True)
