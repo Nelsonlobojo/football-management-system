763342f181325f652e8f7cfa96c261e4c143aa82
Sidney Kanyi <sidney.kanyi@strathmore.edu>
2021-08-29 10:05:22 +0300

    completed units.html

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ app.secret_key = 'bacon'
 # Enter your database connection details below
 app.config['MYSQL_HOST'] = 'localhost'
 app.config['MYSQL_USER'] = 'root'
-app.config['MYSQL_PASSWORD'] = ''
+app.config['MYSQL_PASSWORD'] = '#Billionare27'
 app.config['MYSQL_DB'] = 'teams'
 
 # Intialize MySQL
